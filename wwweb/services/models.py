1c2f3df03bfc120768a8ae17af6b3f195ed0011f
frankiyonki <llonchfrank@gmail.com>
2024-12-02 21:30:35 +0100

    comitaco 2dec

--- wwweb/services/models.py
+++ wwweb/services/models.py
@@ -8,13 +8,16 @@ class AdditionalServiceInfo(models.Model):
         return f"Info for {self.service.title}"  # Ensure no recursion issues
     
 
+
 class Service(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     subtitle = models.CharField(max_length=200, verbose_name="Subtítulo")
     content = models.TextField(verbose_name="Contenido")
-    image = models.ImageField(verbose_name="Imagen", upload_to="services")
+    image = models.ImageField(verbose_name="Imagen", upload_to="services/images")
+    image_hover = models.ImageField(verbose_name="Hover Imagen", upload_to="services/images", blank=True, null=True)  # Second image for hover
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio", default=0.00)
 
     class Meta:
         verbose_name = "servicio"
@@ -23,5 +26,3 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
-
-
--- wwweb/services/urls.py
+++ wwweb/services/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.services, name="services"),
-    path("store/", views.store, name="store"),
     path("product/<int:pk>/", views.product_detail, name="product_detail"),
 ]
